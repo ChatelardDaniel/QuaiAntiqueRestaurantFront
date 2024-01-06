8ea3e961b38e27ee11bda020c0b3519ffc20b3ff
ChatelardDaniel <cdanasc@hotmail.fr>
2024-01-06 18:05:09 +0100

    Création des fichiers pour le routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route.js";
+
+// Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "pages/home.html")
+];
+
+// Le titre s'affiche comme ceci dans l'onglet du navigateur : Route.titre - webitename
+export const webitename = "Quai Antique";
