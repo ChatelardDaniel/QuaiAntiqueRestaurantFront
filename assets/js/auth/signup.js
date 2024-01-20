63d6b0b7cc4f092aaf8241b92d5e62c08044d1bd
ChatelardDaniel <cdanasc@hotmail.fr>
2024-01-20 10:39:10 +0100

    Vérification des autres champs

--- assets/js/auth/signup.js
+++ assets/js/auth/signup.js
@@ -1,10 +1,20 @@
 // implémenter le JS de ma page.
 
 const inputNom = document.getElementById("NomInput");
+const inputPreNom = document.getElementById("PrenomInput");
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const inputValidationPassword = document.getElementById("ValidatePasswordInput");
+
 inputNom.addEventListener("keyup", validateForm);
+inputPreNom.addEventListener("keyup", validateForm);
+inputMail.addEventListener("keyup", validateForm);
+inputPassword.addEventListener("keyup", validateForm);
+inputValidationPassword.addEventListener("keyup", validateForm);
 
 function validateForm() {
     validateRequired(inputNom);
+    validateRequired(inputPreNom);
 }
 
 function validateRequired(input) {
