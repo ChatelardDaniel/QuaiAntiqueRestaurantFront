884df861b4edde7e08f320d88a0a48b23956e3c9
ChatelardDaniel <cdanasc@hotmail.fr>
2024-01-24 20:15:47 +0100

    connexion et token

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,9 +26,9 @@ function setCookie(name,value,days){
 function getCookie(name){
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++){
+    for(var i=0; i < ca.length; i++){
         var c = ca[i];
-        while(c.charAt(0) == ' ') c = c.substring(1,c.length);
+        while(c.charAt(0)==' ') c = c.substring(1,c.length);
         if(c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
     return null;
@@ -36,5 +36,5 @@ function getCookie(name){
 
 // supprimer un cookie.
 function eraseCookie(name){
-    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:00 GMT;';
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
